584c1698a0178f56a49f042189f640f58a867ebf
shreyasmene06 <shreyasmene6@gmail.com>
2024-06-22 00:43:18 +0530

    Update Task1.py
    added comments

--- Task1.py
+++ Task1.py
@@ -5,31 +5,31 @@ command-line or GUI-based application using Python, allowing
 users to create, update, and track their to-do lists'''
 
 tasks=[]
-def Add_task():
+def Add_task():                                     #adds a task in the to-do list
     Input=input("Enter a Task : ")
     tasks.append(Input)
     print("Task Added Successfully !")
 
 def Del_task():
     rem=int(input("Enter Task no. To Be Deleted : "))
-    if rem>len(tasks):
+    if rem>len(tasks):                              #checks if the entered value to be deleted is within the to do list or not
         print("Invalid Task no.")
     elif len(tasks)==0:
         print("No Tasks To Delete")
     else:
         tasks.pop(rem-1)
-        print("Task Removed Succesfully !")
+        print("Task Removed Succesfully !")         #deletes the selected task in the to do list
 
 def Edit_task():
     x=int(input("Enter Task no. To Be Editted : "))
-    if x>len(tasks):
+    if x>len(tasks):                                 #checks if the entered value to be deleted is within the to do list or not
         print("Invalid Task No.")
     elif len(tasks)==0:
         print("No Tasks To Edit")
     else:
         edit=input("Enter Editted Task")
         tasks[x]=edit
-        print("Task Editted Successfully !")
+        print("Task Editted Successfully !")        #edits the selected task in the to do list
 
 def View_task():
   print("\n=====TASKS=====")
@@ -37,7 +37,7 @@ def View_task():
     print("No Tasks Found")
   else:
     for i in range(len(tasks)):
-      print(f'{i+1}. {tasks[i]}')  
+      print(f'{i+1}. {tasks[i]}')                   #lists all the tasks in the list
 
 
 def main():
@@ -45,7 +45,7 @@ def main():
   while True:
     print("\n====== TO DO LIST APPLICATION =====")
     print("1. Add Task")
-    print("2. Remove Task")
+    print("2. Remove Task")                         #main body which joins all different functions and make a interaction with user
     print("3. Edit Task")
     print("4. View Tasks")
     print("5. Exit")
