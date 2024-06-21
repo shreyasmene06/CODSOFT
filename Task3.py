b32a6ed700852c0bbebe67e1133bba92eb86b4a9
shreyasmene06 <shreyasmene6@gmail.com>
2024-06-22 00:52:02 +0530

    Update Task3.py
    added comments

--- Task3.py
+++ Task3.py
@@ -1,20 +1,20 @@
 import random
-def pass_gen(l):
+def pass_gen(l):                        #used to generate random password
     upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     lower=upper.lower()
     special="!@#$%^&*()_+"
     digit="1234567890"
     password=""
-    character="".join(upper+lower+special+digit+special)
+    character="".join(upper+lower+special+digit+special)        #characters adds all different types of values
         
-    for i in range(l):
-        password=password+random.choice(character)
+    for i in range(l):    
+        password=password+random.choice(character)                #chooses password from the combination of characters
         if len(password)>l:
             break
     return password
 
 def main():
-    l=eval(input("Enter length of Password : "))
+    l=eval(input("Enter length of Password : "))                #main body which acts as a user interface for user and connects all functions while making sure length is within range
     if type(l)==int and l>0:
         print(pass_gen(l))
     else:
