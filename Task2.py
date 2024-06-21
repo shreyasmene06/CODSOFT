c9445a10d2b722e7fc19aba9975766b37d030902
shreyasmene06 <shreyasmene6@gmail.com>
2024-06-22 00:49:41 +0530

    Update Task2.py
    added comments

--- Task2.py
+++ Task2.py
@@ -1,32 +1,32 @@
 import math
 def addition(num1,num2):
-    add=num1+num2
+    add=num1+num2                 #performs addition 
     return add
 
 def subtraction(num1,num2):
-    subs=num1-num2
+    subs=num1-num2                #performs subtractiom
     return subtraction
 
 def multiplication(num1,num2):
-    mul=num1*num2
+    mul=num1*num2                 #performs multiplication
     return mul
 
 def division(num1,num2):
-    if num2==0:
+    if num2==0:                    #checks if the division is defined or not (should not be divisible by 0)
         print("NOT DEFINED")
     else:
-        div=num1/num2
+        div=num1/num2             #performs division
         return div
 
 def modulus(num1,num2):
-    mod=num1%num2
+    mod=num1%num2                  #performs modulus 
     return mod
 
 def power(num1,num2):
-    math.pow(num1,num2)
+    math.pow(num1,num2)            #performs exponential operation
     return pow
 
-def trigonometric(num):
+def trigonometric(num):            #gives user option to choose the trigonometric value they want to perform
     print("\n===CHOOSE TRIGNOMETRIC IDENTITIY===")
     print("1.SIN")
     print("2.COS")
@@ -51,17 +51,17 @@ def trigonometric(num):
     else:
         print("Incorrect Choice")
 
-def logarithmic(num):
+def logarithmic(num):            #performs logarthmic functions
     log=math.log(num)
     return log
 
-def sqrt(num):
+def sqrt(num):                    #takes out square root
     return math.sqrt(num)
 
-def cbrt(num):
+def cbrt(num):                    #takes out cube root
     return math.cbrt(num)
-
-def main():
+    
+def main():                        #main body which joins all the functions and acts as a interface for user
     while True:
         print("\n=====CALCULATOR=====")
         print("1.ADDITION")
